2def31ce452ffb9326d6897a3419e048b04b9f0e
Adriana <adrianascosta9@gmail.com>
2022-10-04 16:12:58 -0300

    exerciciocompleto

---
+++ script.js
@@ -0,0 +1,21 @@
+function adicionaItem(event) {
+    event.preventDefault()
+    const main = document.getElementById("container")  ///tag
+    const novoArticle = document.createElement("article") 
+    novoArticle.onclick = function removeItem(event) {
+        event.target.remove()
+    }
+    novoArticle.classList.add("item")   
+    novoArticle.innerHTML = "oii"   
+    main.appendChild(novoArticle)  // adiciona texto item
+    novoArticle.insertAdjacentElement("beforeend")   
+}
+
+
+function removeItem(event) {
+    event.target.remove()
+}
+
+
+
+
